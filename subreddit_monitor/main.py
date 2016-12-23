5adf4f2fefe80ff8d6f7949759e49803e6a1f97b
jbeagley52 <jonathan@jonathanbeagley.com>
2016-12-23 22:43:41 +1100

    Added notifier functions
    - For Mac OS X 10.8 and above with Pync
    - For Linux/Gnome with Libnotify (untested)

--- subreddit_monitor/main.py
+++ subreddit_monitor/main.py
@@ -4,16 +4,34 @@
 import os
 import time
 import traceback
+import platform
 
+from pprint import pprint
 import requests
 import xmltodict
+
+def notify_wrapper(title, subtitle, url):
+    if platform.system() == 'Linux':
+        from gi.repository import Notify
+        Notify.init("Subreddit Monitor")
+        notification = Notify.Notification.new(title,
+                                                subtitle)
+        notification.show()
+        Notify.uninit()
+    if platform.system() == 'Darwin':
+        from pync import Notifier
+        Notifier.notify(title,
+                        title=subtitle,
+                        open=url)
+        Notifier.remove(os.getpid())
+        Notifier.list(os.getpid())
+    else:
+        print("You appear to be using an unsupported OS.")
 
 if __name__ == "__main__":
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     id_path = os.path.join(current_dir, "newest_id.txt")
-    print(current_dir)
-    print(id_path)
 
     current_id = None
     try:
@@ -22,9 +40,6 @@ if __name__ == "__main__":
     except:
         pass
 
-    print("Current ID: {}".format(current_id))
-    print("Monitoring")
-
     subreddit = "python"
     url = "https://www.reddit.com/r/{}/new/.rss".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -32,16 +47,22 @@ if __name__ == "__main__":
     while True:
         try:
             response = requests.get(url, headers=headers)
+            #print(response.text)
             rss = xmltodict.parse(response.text)
 
             newest_entry = rss['feed']['entry'][0]
+            print(newest_entry)
             timestamp = newest_entry['updated']
             newest_id = newest_entry['id']
+            entry_url = newest_entry['link']['@href']
+            content = newest_entry['content']['#text']
             title = newest_entry['title']
 
             if current_id != newest_id:
                 current_id = newest_id
-                print("{} : {}".format(title[:40], current_id))
+                notify_wrapper(title, content[:100], entry_url)
+
+                #print("{} : {}".format(title[:40], current_id))
                 with open(id_path, 'w') as f:
                     f.write(current_id)
 
