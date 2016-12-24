2c6d06947e323296a1e33ac587d35e93271d3083
jbeagley52 <jonathan@jonathanbeagley.com>
2016-12-24 12:48:18 +1100

    Added notifier
    Commit changes I forgot last time

--- subreddit_monitor/main.py
+++ subreddit_monitor/main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 import os
 import time
 import traceback
 import platform
 
-from pprint import pprint
+from HTMLParser import HTMLParser
 import requests
 import xmltodict
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 def notify_wrapper(title, subtitle, url):
     if platform.system() == 'Linux':
@@ -30,48 +45,46 @@ def notify_wrapper(title, subtitle, url):
 
 if __name__ == "__main__":
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    id_path = os.path.join(current_dir, "newest_id.txt")
-
-    current_id = None
-    try:
-        with open(id_path, 'r') as f:
-            current_id = f.read()
-    except:
-        pass
-
-    subreddit = "python"
-    url = "https://www.reddit.com/r/{}/new/.rss".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # id_path = os.path.join(current_dir, "newest_id.txt")
+    #
+    # current_id = None
+    # try:
+    #     with open(id_path, 'r') as f:
+    #         current_id = f.read()
+    # except:
+    #     pass
     while True:
-        try:
-            response = requests.get(url, headers=headers)
-            #print(response.text)
-            rss = xmltodict.parse(response.text)
 
-            newest_entry = rss['feed']['entry'][0]
-            print(newest_entry)
-            timestamp = newest_entry['updated']
-            newest_id = newest_entry['id']
-            entry_url = newest_entry['link']['@href']
-            content = newest_entry['content']['#text']
-            title = newest_entry['title']
+        subreddits = ['python', 'worldnews', 'relationships', 'iama']
+        for subreddit in subreddits:
+            url = "https://www.reddit.com/r/{}/new/.rss".format(subreddit)
+            headers = {'User-Agent': 'Mozilla/5.0'}
 
-            if current_id != newest_id:
+            try:
+                response = requests.get(url, headers=headers)
+                rss = xmltodict.parse(response.text)
+
+                newest_entry = rss['feed']['entry'][0]
+                timestamp = newest_entry['updated']
+                newest_id = newest_entry['id']
+                entry_url = newest_entry['link']['@href']
+                content = strip_tags(newest_entry['content']['#text'])
+                title = strip_tags(newest_entry['title'])
+
+                #if current_id != newest_id:
                 current_id = newest_id
                 notify_wrapper(title, content[:100], entry_url)
 
-                #print("{} : {}".format(title[:40], current_id))
-                with open(id_path, 'w') as f:
-                    f.write(current_id)
+                #with open(id_path, 'w') as f:
+                #    f.write(current_id)
 
-        except Exception as e:
-            traceback.print_exc()
-            try:
-                print(response.status_code)
-                print(response.text)
-            except:
-                pass
-        finally:
-            time.sleep(60 * 5)
+            except Exception as e:
+                traceback.print_exc()
+                try:
+                    print(response.status_code)
+                    print(response.text)
+                except:
+                    pass
+            finally:
+                time.sleep(60 * 0.25)
